10d59e5a6c3a406a7cc6468ea926b9eb8182444f
Nikita Nova <nikita.novikov03@gmail.com>
2020-10-28 14:43:21 -0500

    implementing webm codec/filetype

--- index.js
+++ index.js
@@ -3,36 +3,46 @@ console.log('test')
 navigator.mediaDevices.getUserMedia({audio:true})
     .then(stream =>{
 
-    let audio;
-    const mediaRecorder = new MediaRecorder(stream)
-    let audioChunks = [];
+        let audio;
+        let options = {
+            mimeType: "audio/webm",
+            audioBitsPerSecond: 768000
+        }
+        const mediaRecorder = new MediaRecorder(stream, options)
+        let audioChunks = [];
 
-    document.querySelector("input[name='record']")
-        .addEventListener("click",function(){
-            console.log("record")
-            mediaRecorder.start()
+        document.querySelector("input[name='record']")
+            .addEventListener("click",function(){
+                console.log("record")
+                mediaRecorder.start()
+            })
+
+        mediaRecorder.addEventListener("dataavailable", event=>{
+            console.log("collect data")
+            audioChunks.push(event.data)
+            const audioBlob = new Blob(audioChunks)
+            const audioURL = URL.createObjectURL(audioBlob)
+            audio = new Audio(audioURL)
+            audio.setAttribute("controls","")
+            audio.setAttribute("type","audio/webm")
+            document.querySelector("body").append(audio)
+
         })
 
-    mediaRecorder.addEventListener("dataavailable", event=>{
-        console.log("collect data")
-        audioChunks.push(event.data)
-        const audioBlob = new Blob(audioChunks)
-        const audioURL = URL.createObjectURL(audioBlob)
-        audio = new Audio(audioURL)
-    })
+        document.querySelector("input[name='stop']")
+            .addEventListener("click",function(){
+                console.log("stop")
+                mediaRecorder.stop()
+                audioChunks = []
+                console.log(mediaRecorder)
 
-    document.querySelector("input[name='stop']")
-        .addEventListener("click",function(){
-            console.log("stop")
-            mediaRecorder.stop()
-            audioChunks = []
-    })
+            })
 
-    document.querySelector("input[name='play']")
-        .addEventListener("click",function(){
-            console.log("play")
-            audio.play()
-        })
-
-
+        document.querySelector("input[name='play']")
+            .addEventListener("click",function(){
+                console.log("play")
+                console.log(audio)
+                sessionStorage.setItem("audioMessage",audio)
+                audio.play()
+            })
 })
